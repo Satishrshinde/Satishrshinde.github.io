913021fb32e69bd39c42c455add5d0345fc9671e
visha15l <vshinde@rxsense.com>
2021-12-20 11:39:36 +0530

    Added db json file and config file for api variables.

---
+++ src/config/development.js
@@ -0,0 +1,5 @@
+const DOMAIN = "http://localhost:3003";
+export const API = {
+  TODO_END_POINT: `${DOMAIN}/todo`,
+  CLOTH_END_POINT: `${DOMAIN}/cloths`
+};
---
+++ src/config/production.js
@@ -0,0 +1,5 @@
+const DOMAIN = "https://satish-react-app-backend.herokuapp.com";
+export const API = {
+  TODO_END_POINT: `${DOMAIN}/todo`,
+  CLOTH_END_POINT: `${DOMAIN}/cloths`
+};
