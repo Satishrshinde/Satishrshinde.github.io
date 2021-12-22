b5bb0b93ebcefea20e2b08e5a19f27e510e1ee5c
visha15l <vshinde@rxsense.com>
2021-12-23 00:46:03 +0530

    Added grocery api endpoint.

--- src/config/development.js
+++ src/config/development.js
@@ -1,5 +1,6 @@
 const DOMAIN = "http://localhost:3003";
 export const API = {
   TODO_END_POINT: `${DOMAIN}/todo`,
-  CLOTH_END_POINT: `${DOMAIN}/cloths`
+  CLOTH_END_POINT: `${DOMAIN}/cloths`,
+  GROCERY_END_POINT: `${DOMAIN}/grocery`
 };
